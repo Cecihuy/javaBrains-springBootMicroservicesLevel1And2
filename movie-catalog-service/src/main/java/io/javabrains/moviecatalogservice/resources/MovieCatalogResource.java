524633d39439152ae85ac1de08b9181bc8a488ad
gitKraken <gitkraken@mail.id>
2024-03-13 05:25:35 -0400

    13 Using WebClient to make API calls

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 import io.javabrains.moviecatalogservice.models.CatalogItem;
 import io.javabrains.moviecatalogservice.models.Movie;
 import io.javabrains.moviecatalogservice.models.Rating;
@@ -16,17 +17,25 @@ import io.javabrains.moviecatalogservice.models.Rating;
 public class MovieCatalogResource {
     @Autowired
     private RestTemplate restTemplate;
+    @Autowired
+    private WebClient.Builder webClientBuilder;
 
     @RequestMapping(path = "/{userId}")
-    public List<CatalogItem> getCatalog(@PathVariable(name = "userId") String userId){        
-        
+    public List<CatalogItem> getCatalog(@PathVariable(name = "userId") String userId){
+
         List<Rating> ratings =  Arrays.asList(
             new Rating("1234", 4),
             new Rating("5678", 3)
         );
         return ratings.stream()
             .map(rating -> {
-                Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+                // Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+                Movie movie = webClientBuilder.build()
+                    .get()
+                    .uri("http://localhost:8082/movies/" + rating.getMovieId())
+                    .retrieve()
+                    .bodyToMono(Movie.class)
+                    .block();
                 return new CatalogItem(movie.getName(), "Desc", rating.getRating());
             })
             .collect(Collectors.toList());
