78d7a5d77bbbfab8037cf4265a0bfd48ab4b3094
gitKraken <gitkraken@mail.id>
2024-03-13 05:39:10 -0400

    14 Making a call to the Ratings API

--- ratings-data-service/src/main/java/io/javabrains/ratingsdataservice/resources/RatingsResource.java
+++ ratings-data-service/src/main/java/io/javabrains/ratingsdataservice/resources/RatingsResource.java
@@ -1,4 +1,6 @@
 package io.javabrains.ratingsdataservice.resources;
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,5 +12,13 @@ public class RatingsResource {
     @RequestMapping(path = "/{movieId}")
     public Rating getRating(@PathVariable(name = "movieId") String movieId){
         return new Rating(movieId, 4);
+    }
+    @RequestMapping(path = "/users/{userId}")
+    public List<Rating> getUserRating(@PathVariable(name = "userId") String userId){
+        List<Rating> ratings =  Arrays.asList(
+            new Rating("1234", 4),
+            new Rating("5678", 3)
+        );
+        return ratings;
     }
 }
