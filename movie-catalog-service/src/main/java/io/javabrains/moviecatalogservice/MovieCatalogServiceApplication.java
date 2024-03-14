d3bff6380a868e7a8a9fae247edb37f2d6959049
gitKraken <gitkraken@mail.id>
2024-03-14 00:09:04 -0400

    20 Creating Eureka clients

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -1,10 +1,12 @@
 package io.javabrains.moviecatalogservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MovieCatalogServiceApplication {
 	@Bean
 	public RestTemplate getRestTemplate(){
